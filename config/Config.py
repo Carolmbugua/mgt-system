ea5be1a6d26167bb2f063015c3e8da7ab60ac3a4
carolmt <mbuguacarol5@gmail.com>
2019-12-19 20:37:41 +0300

    make it better

--- config/Config.py
+++ config/Config.py
@@ -1,4 +1,4 @@
 class Develpoment():
-    SQLAlCHEMY_DATABASE_URI='postgresql://postgres:#0724246005@127.0.0.1:5432/sales_demo'
+    SQLAlCHEMY_DATABASE_URI='postgresql://postgres:#0724246005@127.0.0.1:62760/sales_demo'
     SECRET_KEY='65c9a73ac461c4e5ab41f0b21832432d'
     DEBUG=True
